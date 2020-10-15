022c696bc95ce1c47cc7f57ea1d7d7281c56cc14
DmitriyIvanovIT <strela199720@gmail.com>
2020-10-15 12:30:09 +0300

    Updates

---
+++ precache-manifest.a4294cefda09bf4bbca63b4318ee3b9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "985dc8fd0c94e16133e249bdd284a839",
+    "url": "/SpaceX-react/index.html"
+  },
+  {
+    "revision": "b4239379b8c4049a74eb",
+    "url": "/SpaceX-react/static/css/main.2380bc7f.chunk.css"
+  },
+  {
+    "revision": "fed4769287169bc5d2bc",
+    "url": "/SpaceX-react/static/js/2.2a852aa8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/SpaceX-react/static/js/2.2a852aa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4239379b8c4049a74eb",
+    "url": "/SpaceX-react/static/js/main.6976080a.chunk.js"
+  },
+  {
+    "revision": "866075a32f63c23131c1",
+    "url": "/SpaceX-react/static/js/runtime-main.f481c736.js"
+  },
+  {
+    "revision": "d5ab31ce033fc418fd1af87f7b934e1b",
+    "url": "/SpaceX-react/static/media/logo.d5ab31ce.svg"
+  }
+]);
